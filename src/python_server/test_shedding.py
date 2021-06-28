f0cd01eb9e6eb3b7859bebb0dbc78fdb977c0870
Ahmad Slo <ahmad.slo@ipvs.uni-stuttgart.de>
2021-06-28 12:40:22 +0200

    add script to these the correctness of the trained model

---
+++ src/python_server/test_shedding.py
@@ -0,0 +1,68 @@
+import os
+import sys
+from pathlib import Path
+from configobj import ConfigObj
+import argparse
+import glob
+
+import capnp
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(script_dir, "../"))
+from model import build_model
+import mapping_features
+import csv
+
+from capnp_serial import mapping_capnp
+
+
+def shed(properties_file, drop_ratio, ls_result_path):
+    config = ConfigObj(properties_file)
+    fullHistogramBinSize = int(config["fullHistogramBinSize"])
+    featureCorrespondingBinSize = list(map(int, config["featureCorrespondingBinSize"]))
+    trainingDataPath = config["trainingDataPath"]
+    generatedModelPath = config["generatedModelPath"]
+
+    # initialize shedding
+    build_model.init_shedding(generatedModelPath, featureCorrespondingBinSize)
+
+    # load features
+    observations = mapping_features.load_training_data(trainingDataPath, fullHistogramBinSize)
+
+    # get the utility threshold
+    utility_threshold = build_model.get_utility_threshold(drop_ratio)
+
+    # create the result directory if not exists
+    Path(ls_result_path).mkdir(parents=True, exist_ok=True)
+    # open a file to save shedding results
+    f = open(os.path.join(ls_result_path, "shedding_results.csv"), 'w')
+    csv_writer = csv.writer(f)
+    # write the header
+    csv_writer.writerow(['threshold', 'utility', 'shed_decision', 'useful_gt'])
+    number_of_frames= 0
+    number_of_dropped_frames= 0
+    for observation in observations:
+        utility = build_model.get_utility(observation[:-1])
+        if utility > utility_threshold:  # keep
+            csv_writer.writerow([utility_threshold, utility, False, bool(observation[-1])])
+        else:  # shed
+            csv_writer.writerow([utility_threshold, utility, True, bool(observation[-1])])
+            number_of_dropped_frames +=1
+        number_of_frames +=1
+
+    f.close()
+
+    print ("The drop ratio = " + str (number_of_dropped_frames/ number_of_frames) )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--properties_file", dest="properties_file", required=True, type=str,
+                        help="Path to the properties file.")
+    parser.add_argument("-d", "--drop_ratio", dest="drop_ratio", required=True, type=float,
+                        help=" the drop ratio.")
+    parser.add_argument("-r", "--result_path", dest="ls_result_path", required=True, type=str,
+                        help="Path to save the shedding result.")
+
+    args = parser.parse_args()
+
+    shed(args.properties_file, args.drop_ratio, args.ls_result_path)
