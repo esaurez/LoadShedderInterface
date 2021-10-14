070da1a634f07acc43ebb9ad5190d2ab816834f2
Ubuntu <surveillance@openfaas-3.4uyyj4ueav4e1obf54otnimtlc.bx.internal.cloudapp.net>
2021-10-14 15:37:59 +0000

    More documentation in run_full_test_experiment.sh

--- src/python_server/run_full_test_experiment.sh
+++ src/python_server/run_full_test_experiment.sh
@@ -1,22 +1,35 @@
+set -o xtrace
+
 # iterates over: 
 # mbs: minimum bin size (required share of all samples per bin)
 # mode: all_colors or max_cdf
 # ratio: shedding ratio
 
-query='and' # set the correct path to the respective testing and training data in the hsb_configs_template!!! 
+query='red' # set the correct path to the respective testing and training data in the hsb_configs_template!!! 
 
 for mbs in  0.01 0.05 0.1 0.15 0.2 0.25 0.3 ; do # 0.15 0.2; do
-    for mode in 'random'; do #'max_cdf'; do
-        # generate title
+    for mode in 'max_cdf' 'random'; do #'random'; do
+        # Title for the Load Shedder model
         title="${query}_${mbs}_${mode}"
+
+        # The generate_hsb_config.sh script generates the config file 
+        # Each config file has a different Min Bin Size (MBS) and Mode
         ./generate_hsb_config.sh $mode $title $mbs
-        python3 request_handler.py configs/hsb_config_$title.properties # trains and stores model
+
+        # Trains the Utility Function model and stores it.
+        # Stored model contains 
+        #   - per-color split values (bins)
+        #   - utility of each bin
+        #   - mapping from drop rate to utility threshold
+        python3 request_handler.py configs/hsb_config_$title.properties
+
+        # Now analyze the performance of load shedder with different ratios
         for ratio in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
-                python3 test_shedding.py -p configs/hsb_config_$title.properties  -d $ratio -r ../test_results/$query/$ratio/$mbs/$mode
+            # IMPORTANT : This Python script can fail when run for the 1st time, because files are not setup. Rerunning seems to solve that error.
+            python3 test_shedding.py -p configs/hsb_config_$title.properties  -d $ratio -r ../test_results/$query/$ratio/$mbs/$mode
         done
     done
 done
-
 
 # -sr shedding ratios used in the loop
 # -mbs minimum bin sizes 
