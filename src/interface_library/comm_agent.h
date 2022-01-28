d657627e41322f74a4905fa19e79a2353a6c57a1
Enrique Saurez <esaurezapuy3@gatech.edu>
2022-01-28 19:34:38 +0000

    fix comm agent library

--- src/interface_library/comm_agent.h
+++ src/interface_library/comm_agent.h
@@ -7,22 +7,23 @@
 #include <capnp/serialize-packed.h>
 #include <memory>
 #include <mutex>
+#include <unordered_map>
 #include "mapping.capnp.h"
 #include "messages.capnp.h"
 
 class AbstractCommAgent {
 public:
     virtual ~AbstractCommAgent() = default;
-    virtual double getUtilityThreshold(float dropRatio, const std::string& mode = "max_cdf") = 0;
-    virtual double getUtilityValue(Features::Builder &utilityRequest, const std::string& mode = "max_cdf") = 0;
+    virtual std::unordered_map<int, double> getUtilityThreshold(const std::unordered_map<int, float>& perVideoDropRatio, const std::string& mode = "max_cdf") = 0;
+    virtual double getUtilityValue(Features::Builder &utilityRequest, int videoIdx, const std::string& mode = "max_cdf") = 0;
 };
 
 class CommAgent : public AbstractCommAgent{
 public:
     CommAgent(const std::string &serverUrl, std::shared_ptr<zmq::context_t> ctxPtr=nullptr);
     ~CommAgent();
-    virtual double getUtilityThreshold(float dropRatio, const std::string& mode = "max_cdf") override;
-    virtual double getUtilityValue(Features::Builder &utilityRequest, const std::string& mode = "max_cdf") override;
+    virtual std::unordered_map<int, double> getUtilityThreshold(const std::unordered_map<int, float>& perVideoDropRatio, const std::string& mode = "max_cdf") override;
+    virtual double getUtilityValue(Features::Builder &utilityRequest, int videoIdx, const std::string& mode = "max_cdf") override;
 private:
     std::mutex agentLock;
     std::shared_ptr<zmq::context_t> ctx;
